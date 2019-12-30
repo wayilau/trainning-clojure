cdae1d6725cab85a34cc7594ea418ad7d44ebf6b
liuweiyi <liuweiyi@cmss.chinamobile.com>
2019-12-30 17:59:49 +0800

    using core.async. like go program.

--- src/async_tea_party/core.clj
+++ src/async_tea_party/core.clj
@@ -7,5 +7,18 @@
 ;有缓冲的
 
 (def tea-channel (async/chan 10))
+(def milk-channel (async/chan 10))
+(def sugar-channel (async/chan 10))
 
+；write to channel
 (async/>!! tea-channel :cup-of-tea)
+
+;get from channel
+(async/<!! tea-channel)
+(async/go-loop []
+  (let [[v ch] (async/alts! [tea-channel
+                             milk-channel
+                             sugar-channel])]
+    (println "Got " v " from " ch)
+    (recur)))
+

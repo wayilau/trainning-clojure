5ce8f366c620eb614ca5b467f710db919e0a98a5
liuweiyi <liuweiyi@cmss.chinamobile.com>
2020-01-13 17:55:03 +0800

    Tests ring & json lib.

--- project.clj
+++ project.clj
@@ -3,14 +3,14 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :main liu.learning.hello
+  ;:main liu.learning.hello
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [clj-time/clj-time "0.14.2"]
                  [compojure "1.6.1"]
                  [ring/ring-defaults "0.3.2"]
                  [camel-snake-kebab "0.2.4"]
-                 [org.clojure/core.async "0.6.532"]]
+                 [org.clojure/core.async "0.6.532"]
+                 [cheshire "5.9.0"]]
   :repl-options {:init-ns clojure-lein.hello}
   :plugins  [[lein-ring "0.12.5"]]
-  :ring {:handler clojure_lein.handler/web})
-
+  :ring {:handler web.handler/app})
--- src/web/handler.clj
+++ src/web/handler.clj
@@ -1,11 +1,23 @@
-(ns web.handler)
-(:require [compojure.core :refer :all]
-  [compojure.route :as route]
-  [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+(ns web.handler
+  (:require [compojure.core :refer :all]
+            [compojure.route :as route]
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
+            [cheshire.core :as json]))
 
 (defroutes app-routes
-           (GET "/" [] "Hello World")
+           "app routes define the api routes. when we use
+           theses apis, we can do a lot things."
+           (GET "/" [] "Hello, World!")
+           (GET "/cheshire-cat" []
+             {:status  200
+              :headers {"Content-type" "application/json; charset=utf-8"}
+              :body    (json/generate-string
+                         {:name   "Cheshire  Cat"
+                          :status :grining})})
            (route/not-found "Not Found"))
 
 (def app
+  "It is a app for test ring plugin."
   (wrap-defaults app-routes site-defaults))
+
+
